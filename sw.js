a43a43733bfc32ae5d800b5a6e548206d2f4ca32
aocastro <adriano.castro2@etec.sp.gov.br>
2020-09-10 13:32:16 -0300

    Commit de melhorias do SW

--- sw.js
+++ sw.js
@@ -1,3 +1,27 @@
+// Declarar as variáveis / constantes necesárias para atuação do nosso service worker
+var CACHE_NAME = 'adrianocastro-cache-v1'
+var urlsToCache = [
+    'css/adrianocastro.css',
+    'css/bootstrap.css',
+    'img/favicon.png',
+    'img/favicon.png',
+    'js/bootstrap.js',
+    'js/jquery-3.5.1.min.js',
+    'js/portifolio.js',
+    'libs/sweetalert2-master/src/sweetalert2.js',
+]
+
+self.addEventListener('install', function(event) {
+    // Paremetrizar as estapas de instalação do nosso cache no dispositivo
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+        .then(function(cache) {
+            console.log('Cache aberto...')
+            return cache.addAll(urlsToCache)
+        })
+    )
+})
+
 self.addEventListener('fetch', function(event) {
 
 })
